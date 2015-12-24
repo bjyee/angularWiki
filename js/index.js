32089813a30dce5448753a27430fb6f617a82d1a
Yee <Brandon.Yee@us.crowncastle.com>
2015-12-24 14:14:21 -0500

    now attempting to do routing to load content

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var app = angular.module("wikiApp", ['ngRoute']);
 
+// Controllers
 app.controller('wikiAppController', function($scope){
     var waCrtl = this;
    
@@ -8,7 +9,7 @@ app.controller('wikiAppController', function($scope){
         id : 1,
         first : "Brandon",
         last : "Yee",
-        email : "brandon.yee.vendor@crowncastle.com",
+        email : "hi",
         password : "abc123"
     } 
     
@@ -16,8 +17,34 @@ app.controller('wikiAppController', function($scope){
     $scope.signedIn = false;
     
     waCrtl.logIn = function(){
-        if (typeof $scope.waCred[waCrtl.email] != 'undefined' && $scope.waCred[waCrtl.email].password == waCrtl.password){
+        if ($scope.waCred.email.toLowerCase() == waCrtl.email.toLowerCase() && $scope.waCred.password == waCrtl.password){
             $scope.signedIn = true;
         }
     }
+});
+
+// Routes config
+app.config(function($routeProvider){
+    $routeProvider
+        .when("/" ,{
+            templateUrl : 'mainPage.html',
+            controller : "mainPageController",
+        })
+        .when("/signUp", {
+            templateUrl : 'signUp.html',
+            controller : "signUpController",
+        })
+        .when("/search/:searchInput", {
+            templateUrl : 'searchResults.html',
+            controller : "searchResultsController",
+        })
+        .when("/article/:articleName", {
+            templateUrl : 'articles.html',
+            controller : "articlesController",
+        })
+        .when("/error/:errorNumber", {
+            templateUrl : 'error.html',
+            controller : "errorController",
+        })
+        
 });
