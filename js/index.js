2ab58c74840e70da5625d7bb47703342a3b5b091
Yee <Brandon.Yee@us.crowncastle.com>
2015-12-24 13:34:52 -0500

    attempting to do a pseudo sign in with a header change

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
 var app = angular.module("wikiApp", ['ngRoute']);
 
+app.controller('wikiAppController', function($scope){
+    var waCrtl = this;
+   
+    // Current user database
+    $scope.waCred = {
+        id : 1,
+        first : "Brandon",
+        last : "Yee",
+        email : "brandon.yee.vendor@crowncastle.com",
+        password : "abc123"
+    } 
+    
+    // are they signed in? not yet
+    $scope.signedIn = false;
+    
+    waCrtl.logIn = function(){
+        if (typeof $scope.waCred[waCrtl.email] != 'undefined' && $scope.waCred[waCrtl.email].password == waCrtl.password){
+            $scope.signedIn = true;
+        }
+    }
+});
