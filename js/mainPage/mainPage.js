fc309ac95b69b70903317ff94a34025bfd8cd90a
Yee <Brandon.Yee@us.crowncastle.com>
2015-12-28 11:42:55 -0500

    in the middle of search functionality on the main page.

--- js/mainPage/mainPage.js
+++ js/mainPage/mainPage.js
@@ -1,2 +1,11 @@
 
+var app = angular.module("main", ['ngRoute']);
 
+// Controllers
+app.controller('mainPageController', function($scope){
+    var mpCrtl = this;
+    
+    mpCrtl.search = function(){
+        
+    }
+});
